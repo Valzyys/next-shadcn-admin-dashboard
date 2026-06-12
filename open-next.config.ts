3cb213128b790df9ae119e8f0d74427975926100
Valzz <jkt48connect@gmail.com>
2026-06-12 23:13:18 +0700

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,25 +1,24 @@
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
-
-export default config;
+{
+	  default: {
+	    override: {
+	      wrapper: "cloudflare-node",
+	      converter: "edge",
+	      proxyExternalRequest: "fetch",
+	      incrementalCache: "dummy" | function,
+	      tagCache: "dummy" | function,
+	      queue: "dummy" | "direct" | function,
+	    },
+	  },
+	  edgeExternals: ["node:crypto"],
+	  middleware: {
+	    external: true,
+	    override: {
+	      wrapper: "cloudflare-edge",
+	      converter: "edge",
+	      proxyExternalRequest: "fetch",
+	      incrementalCache: "dummy" | function,
+	      tagCache: "dummy" | function,
+	      queue: "dummy" | "direct" | function,
+	    },
+	  },
+}
