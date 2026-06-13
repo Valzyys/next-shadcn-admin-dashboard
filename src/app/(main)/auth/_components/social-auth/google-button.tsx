aeabd4fb30b9642aaabe677b0df341be03422bfe
Valzz <jkt48connect@gmail.com>
2026-06-14 02:21:07 +0700

    Update google-button.tsx

--- src/app/(main)/auth/_components/social-auth/google-button.tsx
+++ src/app/(main)/auth/_components/social-auth/google-button.tsx
@@ -1,12 +1,21 @@
 import { siGoogle } from "simple-icons";
-
 import { SimpleIcon } from "@/components/simple-icon";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export function GoogleButton({ className, ...props }: React.ComponentProps<typeof Button>) {
+  const handleClick = () => {
+    window.dispatchEvent(new Event("trigger-google-login"));
+  };
+
   return (
-    <Button variant="secondary" className={cn(className)} {...props}>
+    <Button
+      variant="secondary"
+      className={cn(className)}
+      type="button"
+      onClick={handleClick}
+      {...props}
+    >
       <SimpleIcon icon={siGoogle} className="size-4" />
       Continue with Google
     </Button>
