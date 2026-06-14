f57512612cf7b170d1cf77439fa5f350c7bced49
Valzz <jkt48connect@gmail.com>
2026-06-14 18:29:54 +0700

    Update layout.tsx

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -70,7 +70,7 @@ export default async function Layout({ children }: Readonly<{ children: ReactNod
               <Button asChild size="icon">
                 <Link
                   prefetch={false}
-                  href="https://github.com/arhamkhnz/next-shadcn-admin-dashboard"
+                  href="https://github.com/JKT48Connect"
                   target="_blank"
                   rel="noreferrer"
                   aria-label="Open GitHub repository"
