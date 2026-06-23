a9fdfff9bd86eecda7e3576c3f1ace67284f8c7f
Valzz <jkt48connect@gmail.com>
2026-06-23 22:37:40 +0700

    Create page.tsx

---
+++ src/app/(main)/dashboard/profile/page.tsx
@@ -0,0 +1,9 @@
+import { ProfilePage } from "./_components/profile";
+
+export default function Page() {
+  return (
+    <div data-content-padding="false">
+      <ProfilePage />
+    </div>
+  );
+}
