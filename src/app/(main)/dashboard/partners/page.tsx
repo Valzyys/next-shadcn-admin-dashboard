0b0bbb32168c59566cf5081b1a58c818adb69455
Valzz <jkt48connect@gmail.com>
2026-06-25 02:13:09 +0700

    Create page.tsx

---
+++ src/app/(main)/dashboard/partners/page.tsx
@@ -0,0 +1,9 @@
+import { PartnershipPage } from "./_components/PartnershipPage";
+
+export default function Page() {
+  return (
+    <div data-content-padding="false">
+      <PartnershipPage />
+    </div>
+  );
+}
